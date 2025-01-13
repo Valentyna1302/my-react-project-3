883882f2329aff12b0c0ea98b18b4424be849bdc
Drynkin Sergey <sergsmile29@gmail.com>
2025-01-13 20:03:50 +0200

    base onclick

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,14 +1,29 @@
 import s from './Counter.module.css';
 
 export const Counter = () => {
+  const handleResetClick = name => {
+    console.log(`Hello ${name}!`);
+  };
+
+  const handlePlusClick = e => {
+    console.log('Plus was clicked!');
+    console.log(e);
+  };
+
   return (
     <div className={s.flexContainer}>
       <div className={s.wrapper}>
         <h1>{1}</h1>
         <div className={s.flex}>
-          <button className='btn'>minus</button>
-          <button className='btn'>reset</button>
-          <button className='btn'>plus</button>
+          <button onClick={() => console.log('React is awesome!!!')} className='btn'>
+            minus
+          </button>
+          <button className='btn' onClick={() => handleResetClick('Alex')}>
+            reset
+          </button>
+          <button className='btn' onClick={handlePlusClick}>
+            plus
+          </button>
         </div>
       </div>
     </div>
