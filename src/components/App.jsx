57ae021ec1809bf1d1791a32741726a72fc97a20
Drynkin Sergey <sergsmile29@gmail.com>
2025-01-20 21:31:14 +0200

    finally

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,24 @@
+import ControlledForm from './Forms/ControlledForm';
+import UncontrolledForm from './Forms/UncontrolledForm';
 import Header from './Header/Header';
+import UseIdExample from './UseIdExample/UseIdExample';
 
 const App = () => {
+  const register = credentials => {
+    console.log('Data received!');
+    setTimeout(() => {
+      console.log('Data processing....');
+      setTimeout(() => {
+        console.log(credentials);
+        console.log('User was register successfully!');
+      }, 2000);
+    }, 1000);
+  };
   return (
     <div>
       <Header />
+      <UseIdExample />
+      {/* <ControlledForm register={register} /> */}
     </div>
   );
 };
