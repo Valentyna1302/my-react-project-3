ed8f3be553386ec31081c3331173bc8ad69d98de
Drynkin Sergey <sergsmile29@gmail.com>
2025-02-03 20:34:19 +0200

    Outlet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,9 @@ import Header from './Header/Header';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import NotFound from '../pages/NotFound';
+import Aim from './NestedRoutes/Aim';
+import Team from './NestedRoutes/Team';
+import Company from './NestedRoutes/Company';
 
 const App = () => {
   return (
@@ -10,7 +13,15 @@ const App = () => {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
+        {/* localhost/about/aim */}
+        {/* localhost/about/company */}
+        {/* localhost/about/team */}
+        <Route path='/about' element={<About />}>
+          <Route path='aim' element={<Aim />} />
+          <Route path='company' element={<Company />} />
+          <Route path='team' element={<Team />} />
+        </Route>
+
         <Route path='*' element={<NotFound />} />
       </Routes>
     </main>
