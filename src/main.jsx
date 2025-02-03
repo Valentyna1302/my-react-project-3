a8e786a90e251ce6de03c079ef1d1e0714f4e77e
Drynkin Sergey <sergsmile29@gmail.com>
2025-02-03 18:50:52 +0200

    init m5l1

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,11 @@ import App from './components/App';
 import './index.css';
 import 'modern-normalize';
 import { Toaster } from 'react-hot-toast';
-import { AuthProvider } from './providers/AuthProvider/AuthProvider';
-import { ThemeProvider } from './providers/ThemeProvider/ThemeProvider';
 createRoot(document.getElementById('root')).render(
   <>
-    <ThemeProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ThemeProvider>
-
+    <>
+      <App />
+    </>
     <Toaster position='top-right' toastOptions={{ duration: 1000 }} />
   </>
 );
